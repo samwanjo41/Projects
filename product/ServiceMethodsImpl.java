57e93c517c417c35975fb7eff93a6cb7be7eb962
Samuel Wanjohi <samuel.wanjohi@tracom.co.ke>
2023-05-24 21:38:26 +0300

    modify ServiceMethodsImpl to implement serviceMethods interface

--- product/ServiceMethodsImpl.java
+++ product/ServiceMethodsImpl.java
@@ -1,4 +1,70 @@
 package product;
 
-public class ServiceMethodsImpl {
+import java.util.List;
+
+public class ServiceMethodsImpl implements ServiceMethods{
+    @Override
+    public Product createProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public String deleteProduct(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void addNewToInventory(Product product) {
+
+    }
+
+    @Override
+    public void incrementProductCount(int id, int count) {
+
+    }
+
+    @Override
+    public void decrementProductCount(int id, int count) {
+
+    }
+
+    @Override
+    public void removeFromInventory(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductByName(String productName) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(int id) {
+        return null;
+    }
+
+    @Override
+    public Category addCategory(String name) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(String name) {
+
+    }
+
+    @Override
+    public List<Category> getCategories() {
+        return null;
+    }
 }
